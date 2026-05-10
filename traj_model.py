2cf1c22affb899c90aaa1b57dab7bbb226462cec
Larkteryny <damiendamascus23@gmail.com>
2026-05-10 01:29:22 -0400

    General quadratic trajectory generator

--- traj_model.py
+++ traj_model.py
@@ -11,3 +11,10 @@ def ideal_motion_yint(start: np.ndarray, vel: np.ndarray, target: np.ndarray):
     t = (target[0] - start[0]) / vel[0]
     y = -4.905 * t * t + vel[1] * t + start[1]
     return np.array([target[0], float(y)])
+
+def general_order2(a, b, c):
+    def polynomial(start: np.ndarray, vel: np.ndarray, target: np.ndarray):
+        t = (target[0] - start[0]) / vel[0]
+        y = a * t * t + b * vel[1] * t + start[1] + c
+        return np.array([target[0], float(y)])
+    return polynomial
